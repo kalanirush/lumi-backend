e3d98b01d12c3cafeee171c3259874ee2455825b
kalanirush <kalanirush@gmail.com>
2026-05-28 22:43:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 
 // Trust Railway's proxy
 app.set('trust proxy', 1);
